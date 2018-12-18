f5d98f221324e4118136fa1df7257556197f2050
Paulo Augusto Nogueira Foroni <paulo.foroni@linx.com.br>
2018-12-18 18:38:39 -0200

    alterado footer

--- frontend/src/common/template/footer.jsx
+++ frontend/src/common/template/footer.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 export default props => (
     <footer className='main-footer'>
         <strong>
-            Copyright &copy; 2017
-            <a href='http://cod3r.com.br' target='_blank'> Cod3r</a>.
+            Copyright &copy; 2018
+            <a href='http://pnf.com.br' target='_blank'> Pforoni</a>.
         </strong>
     </footer>
 )
