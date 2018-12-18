0930be6a42a5f4ebcf4aea4ad534e540d705147c
Paulo Augusto Nogueira Foroni <paulo.foroni@linx.com.br>
2018-12-18 14:27:55 -0200

    finalizado back-end

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -3,11 +3,15 @@ const port = 3003
 const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
+const allowCors = require('./cors')
+const queryParser = require('express-query-int')
 
 //interpreta todas as requisições do server quando o formato for urlencoded
 server.use(bodyParser.urlencoded({extended: true}))
 //Parser quando for json
 server.use(bodyParser.json())
+server.use(allowCors)
+server.use(queryParser())
 
 server.listen(port, function(){
     console.log(`BackEnd is running on port ${port}.`)
